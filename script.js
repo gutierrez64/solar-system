069a179875738c75d4b6dbe458364d4dcf800c4f
gutierrez64 <gutierrezmatheus210@gmail.com>
2023-09-25 01:17:21 -0300

    white mode

--- script.js
+++ script.js
@@ -13,4 +13,34 @@ function createStars() {
     }
 }
 
-createStars()
+function changeMode() {
+    console.log(document.querySelector("body").style.backgroundColor)
+    if (document.querySelector("body").style.backgroundColor == "rgb(255, 255, 255)") {
+        document.querySelector("body").style.backgroundColor = "rgb(0, 0, 0)"
+        document.querySelector("i").style.color = "#fff"
+        document.querySelector(".mercury").style.borderColor = "#fff transparent transparent transparent"
+        document.querySelector(".venus").style.borderColor = "#fff transparent transparent transparent"
+        document.querySelector(".earth").style.borderColor = "#fff transparent transparent transparent"
+        document.querySelector(".mars").style.borderColor = "#fff transparent transparent transparent"
+        document.querySelector(".jupiter").style.borderColor = "#fff transparent transparent transparent"
+        document.querySelector(".saturn").style.borderColor = "#fff transparent transparent transparent"
+        document.querySelector(".uranus").style.borderColor = "#fff transparent transparent transparent"
+        document.querySelector(".neptune").style.borderColor = "#fff transparent transparent transparent"
+        document.querySelector(".pluto").style.borderColor = "#fff transparent transparent transparent"
+    }
+    else {
+        document.querySelector("body").style.backgroundColor = "rgb(255, 255, 255)"
+        document.querySelector("i").style.color = "#000"
+        document.querySelector(".mercury").style.borderColor = "#000 transparent transparent transparent"
+        document.querySelector(".venus").style.borderColor = "#000 transparent transparent transparent"
+        document.querySelector(".earth").style.borderColor = "#000 transparent transparent transparent"
+        document.querySelector(".mars").style.borderColor = "#000 transparent transparent transparent"
+        document.querySelector(".jupiter").style.borderColor = "#000 transparent transparent transparent"
+        document.querySelector(".saturn").style.borderColor = "#000 transparent transparent transparent"
+        document.querySelector(".uranus").style.borderColor = "#000 transparent transparent transparent"
+        document.querySelector(".neptune").style.borderColor = "#000 transparent transparent transparent"
+        document.querySelector(".pluto").style.borderColor = "#000 transparent transparent transparent"
+    }
+}
+
+//createStars()
